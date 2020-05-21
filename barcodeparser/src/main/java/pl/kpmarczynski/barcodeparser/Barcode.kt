f6339c0bf93e8c22adc49009bbf519bffc8fa969
kp-marczynski <kp.marczynski@gmail.com>
2020-05-21 17:57:33 +0200

    added merging barcodes

--- barcodeparser/src/main/java/pl/kpmarczynski/barcodeparser/Barcode.kt
+++ barcodeparser/src/main/java/pl/kpmarczynski/barcodeparser/Barcode.kt
@@ -4,16 +4,16 @@ import pl.kpmarczynski.barcodeparser.gs1.Gs1ApplicationIdentifier
 
 data class Barcode(val fields: List<BarcodeField>) {
     fun merge(other: Barcode): Barcode {
-        val raw1 = fields.firstOrNull { it.label == Gs1ApplicationIdentifier.GS1_RAW }
-        val raw2 = other.fields.firstOrNull { it.label == Gs1ApplicationIdentifier.GS1_RAW }
+        val raw1 = fields.firstOrNull { it.label == Gs1ApplicationIdentifier.GS1_RAW }?.getRawData()
+        val raw2 = other.fields.firstOrNull { it.label == Gs1ApplicationIdentifier.GS1_RAW }?.getRawData()
 
-        val malformed1 = fields.firstOrNull { it.label == Gs1ApplicationIdentifier.GS1_MALFORMED }
+        val malformed1 = fields.firstOrNull { it.label == Gs1ApplicationIdentifier.GS1_MALFORMED }?.getRawData()
         return Barcode(listOf(
             BarcodeField(
                 Gs1ApplicationIdentifier.GS1_RAW,
                 "$raw1 $raw2"
             )
-        ) + if (malformed1 == null) {
+        ) + if (malformed1 != null) {
             fields.filter { it.label != Gs1ApplicationIdentifier.GS1_RAW && it.label != Gs1ApplicationIdentifier.GS1_MALFORMED
             } + BarcodeField(Gs1ApplicationIdentifier.GS1_MALFORMED, "$malformed1 $raw2")
         } else {
